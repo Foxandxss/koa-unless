c821cbba6f7dd7a95d01faf760090a32e9214edf
Bruno Krebs <krebs.bruno@gmail.com>
2016-12-21 21:49:41 -0200

    Refactoring wrapper middleware to make it more readable.

--- index.js
+++ index.js
@@ -1,50 +1,97 @@
+/**
+ * Koa unless middleware. Attach to any middleware and configure it to prevent/permit the
+ * middleware in question to be executed.
+ *
+ * @module koa-unless
+ */
+
 var url = require('url');
 
+/** Creates a middleware wrapper that verify if the original middleware should be executed or not. */
 module.exports = function(options) {
   var parent = this;
 
-  var opts = typeof options === 'function' ? {custom: options} : options;
-  opts.useOriginalUrl = (typeof opts.useOriginalUrl === 'undefined') ? true : opts.useOriginalUrl;
+  // If a custom function was passed directly, creates a new object literal that holds it as a property called custom.
+  var opts = typeof options === 'function' ? { custom: options } : options;
 
+  // Returns the middleware that wraps the original one.
   return function *(next) {
     var requestedUrl = url.parse((opts.useOriginalUrl ? this.originalUrl : this.url) || '', true);
 
-    var skip = false;
-
-    if (opts.custom) {
-      skip = skip || opts.custom.call(this);
-    }
-
-    var paths = !opts.path || Array.isArray(opts.path) ?
-                opts.path : [opts.path];
-
-    if (paths) {
-      skip = skip || paths.some(function(p) {
-        return (typeof p === 'string' && p === requestedUrl.pathname) ||
-          (p instanceof RegExp && !! p.exec(requestedUrl.pathname));
-      });
-    }
-
-    var exts = !opts.ext || Array.isArray(opts.ext) ?
-               opts.ext : [opts.ext];
-
-    if (exts) {
-      skip = skip || exts.some(function(ext) {
-         return requestedUrl.pathname.substr(ext.length * -1) === ext;
-      });
-    }
-
-    var methods = !opts.method || Array.isArray(opts.method) ?
-                  opts.method : [opts.method];
-
-    if (methods) {
-      skip = skip || !!~methods.indexOf(this.method);
-    }
-
-    if (skip) {
+    if (matchesCustom(this, opts) || matchesPath(requestedUrl, opts) ||
+        matchesExtension(requestedUrl, opts) || matchesMethod(this.method, opts)) {
       return yield *next;
     }
 
     yield *parent.call(this, next);
   };
 };
+
+/**
+ * Returns boolean indicating whether the custom function returns true.
+ *
+ * @param ctx - Koa context
+ * @param opts - unless configuration
+ * @returns {boolean}
+ */
+function matchesCustom(ctx, opts) {
+  if (opts.custom) {
+    return opts.custom.call(ctx);
+  }
+  return false;
+}
+
+/**
+ * Returns boolean indicating whether the requestUrl matches against the paths configured.
+ *
+ * @param requestedUrl - url requested by user
+ * @param opts - unless configuration
+ * @returns {boolean}
+ */
+function matchesPath(requestedUrl, opts) {
+  var paths = !opts.path || Array.isArray(opts.path) ?
+    opts.path : [opts.path];
+
+  if (paths) {
+    return paths.some(function(p) {
+      return (typeof p === 'string' && p === requestedUrl.pathname) ||
+        (p instanceof RegExp && !! p.exec(requestedUrl.pathname));
+    });
+  }
+
+  return false;
+}
+
+/**
+ * Returns boolean indicating whether the requestUrl ends with the configured extensions.
+ *
+ * @param requestedUrl - url requested by user
+ * @param opts - unless configuration
+ * @returns {boolean}
+ */
+function matchesExtension(requestedUrl, opts) {
+  var exts = !opts.ext || Array.isArray(opts.ext) ?
+    opts.ext : [opts.ext];
+
+  if (exts) {
+    return exts.some(function(ext) {
+      return requestedUrl.pathname.substr(ext.length * -1) === ext;
+    });
+  }
+}
+
+/**
+ * Returns boolean indicating whether the request method matches the configured methods.
+ *
+ * @param requestedUrl - url requested by user
+ * @param opts - unless configuration
+ * @returns {boolean}
+ */
+function matchesMethod(method, opts) {
+  var methods = !opts.method || Array.isArray(opts.method) ?
+    opts.method : [opts.method];
+
+  if (methods) {
+    return !!~methods.indexOf(method);
+  }
+}
