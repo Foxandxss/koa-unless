3477e99e6f7d7d46b232a5ae11b317fd4a7e41c8
Bruno Krebs <krebs.bruno@gmail.com>
2016-12-21 12:50:14 -0200

    Creates a TestRunner class that generate runnable instances. To create a class just pass its constructor a test suite name and a test scenario path. After that, just call runTests and its tests will be run.

---
+++ test/test-runner.class.js
@@ -0,0 +1,35 @@
+const unless  = require('../index');
+const koa = require('koa');
+const request = require('supertest');
+
+module.exports = class TestRunner {
+  constructor(testName, scenarios) {
+    this.testName = testName;
+    this.SCENARIO_PATHS = scenarios;
+
+    this.middleware = function *() {
+      this.body = { executed: true };
+    };
+    this.middleware.unless = unless;
+  }
+
+  runTests() {
+    let instance = this;
+
+    describe(instance.testName, function () {
+
+      instance.SCENARIO_PATHS.forEach((scenario) => {
+        let acceptDeny = scenario.expected == 200 ? 'accept' : 'deny';
+
+        it(`should ${acceptDeny} access to ${scenario.testSample} when configured with: ${scenario.path}`, function (done) {
+          let app = koa();
+
+          app.use(instance.middleware.unless({ path: scenario.path }));
+          request(app.listen())
+            .get(scenario.testSample)
+            .expect(scenario.expected, done);
+        });
+      });
+    });
+  }
+};
